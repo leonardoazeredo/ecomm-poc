5f3403291124366e39c624d197575566a8c45556
Leonardo <leonardo.azeredo@live.com>
2025-04-27 00:02:43 -0300

    style: Updated copyright message and link styles for enhanced consistency.
    - Update © message in footer component
    - Update link styles in Header.tsx to improve consistency and aesthetics.

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-black border-t mt-12">
-      <div className="container mx-auto px-4 py-6 text-center text-gray-600 text-sm">
+      <div className="container mx-auto px-4 py-6 text-center text-sm">
         © {currentYear} MyEcommStore. All rights reserved.
       </div>
     </footer>
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,24 +4,15 @@ export default function Header() {
   return (
     <header className="bg-black shadow-md sticky top-0 z-50">
       <nav className="container mx-auto px-4 py-3 flex justify-between items-center">
-        <Link
-          href="/"
-          className="text-xl font-bold text-gray-800 hover:text-blue-600"
-        >
+        <Link href="/" className="text-xl font-bold  hover:text-gray-400">
           MyEcommStore
         </Link>
 
         <div className="flex items-center space-x-6">
-          <Link href="/" className="text-gray-600 hover:text-gray-900">
-            Home
-          </Link>
-          <Link href="/products" className="text-gray-600 hover:text-gray-900">
+          <Link href="/products" className="hover:text-gray-400">
             Products
           </Link>
-          <Link
-            href="/cart"
-            className="text-gray-600 hover:text-gray-900 relative"
-          >
+          <Link href="/cart" className="hover:text-gray-400 relative">
             Cart
           </Link>
         </div>
